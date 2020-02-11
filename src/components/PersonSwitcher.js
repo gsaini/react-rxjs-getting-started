27124e0a04bcbf65ebe3c3d829780617059ddc49
Gopal Saini <gopal.saini.work@gmail.com>
2020-02-11 17:23:29 -0500

    initial commit

---
+++ src/components/PersonSwitcher.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import chatStore from '../store/chat';
+import { Link } from 'react-router-dom';
+
+const PersonSwitcher = () => {
+  const [chatState, setChatState] = useState(chatStore.initialState);
+  const location = window.location.href.split('/')[3];
+
+  useEffect(() => {
+    chatStore.subscribe(setChatState);
+    chatStore.init();
+  }, [])
+
+  const messageNotification = chatState.newDataCount > 0
+    && (<span className="notify">{chatState.newDataCount}</span>);
+
+  return (
+    <div className="switcher-div">
+      <Link to="/first-person"><button className="switcher">
+        Person1
+        {location !== 'first-person' && location.length > 1 && messageNotification}
+      </button></Link>
+      <Link to="/second-person"><button className="switcher">
+        Person2
+        {location !== 'second-person' && messageNotification}        
+      </button></Link>
+    </div>
+  );
+}
+
+export default PersonSwitcher;
---
+++ src/store/chat.js
@@ -0,0 +1,33 @@
+import { Subject } from 'rxjs';
+
+const subject = new Subject();
+
+const initialState = {
+    data: [],
+    newDataCount: 0
+}
+
+let state = initialState;
+
+const chatStore = {
+    init: () => {
+        state = { ...state, newDataCount: 0 };
+        subject.next(state)
+    },
+    subscribe: setState => subject.subscribe(setState),
+    sendMessage: message => {
+        state = {
+            ...state,
+            data: [...state.data, message],
+            newDataCount: state.newDataCount + 1
+        };
+        subject.next(state);
+    },
+    clearChat: () => {
+        state = initialState;
+        subject.next(state);
+    },
+    initialState
+};
+
+export default chatStore;
